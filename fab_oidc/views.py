59d5defb72d28af08b5c2d598a03d6480a481fec
patilv <vinay.x.patil.-nd@espn.com>
2020-10-21 19:49:05 +0530

    Update views.py to enable OIDC role access

--- fab_oidc/views.py
+++ fab_oidc/views.py
@@ -5,13 +5,16 @@ from flask_login import login_user
 from flask_admin import expose
 from urllib.parse import quote
 
-
 # Set the OIDC field that should be used as a username
 USERNAME_OIDC_FIELD = os.getenv('USERNAME_OIDC_FIELD', default='sub')
 FIRST_NAME_OIDC_FIELD = os.getenv('FIRST_NAME_OIDC_FIELD',
                                   default='nickname')
 LAST_NAME_OIDC_FIELD = os.getenv('LAST_NAME_OIDC_FIELD',
                                  default='name')
+ROLES_OIDC_FIELD = os.getenv('ROLES_OIDC_FIELD',
+                             default='roles')
+ENABLE_ROLE_OIDC_ACCESS = os.getenv('ENABLE_ROLE_OIDC_ACCESS',
+                                    default='True')
 
 
 class AuthOIDCView(AuthOIDView):
@@ -27,23 +30,33 @@ class AuthOIDCView(AuthOIDView):
             user = sm.auth_user_oid(oidc.user_getfield('email'))
 
             # Get user roles.
-            user_roles = oidc.user_getfield('roles')
+            user_roles = oidc.user_getfield(ROLES_OIDC_FIELD)
             print(f"----User {oidc.user_getfield('email')} has roles in JWT - {user_roles}")
 
             # Iterate through each role, and check if its available.
             assign_roles = []
-            if user_roles:
-                for role in user_roles:
-                    fetch_role = sm.find_role(role)
-                    if fetch_role:
-                        assign_roles.append(fetch_role)
+            if ENABLE_ROLE_OIDC_ACCESS.lower() in ['true']:
+                print(f"----Role based OIDC access is ENABLED...")
+                if user_roles:
+                    for role in user_roles:
+                        fetch_role = sm.find_role(role)
+                        if fetch_role:
+                            assign_roles.append(fetch_role)
+            else:
+                print(f"----Role based OIDC access is DISABLED...")
+                if sm.auth_user_registration_role:
+                    user_reg_role = sm.find_role(sm.auth_user_registration_role)
+                    if user_reg_role:
+                        assign_roles.append(sm.find_role(sm.auth_user_registration_role))
+                    else:
+                        print(f"----User registration role is None...")
 
             # Thrown 401 if no roles are assigned to the user
             if len(assign_roles) == 0:
                 print(f"No role available for the user {oidc.user_getfield('email')} for access...")
                 return render_template_string("Unauthorized Access! Please contact administrator...")
-            else:
-                print(f"----Roles available for the user {oidc.user_getfield('email')} - {assign_roles}")
+
+            print(f"----Roles available for the user {oidc.user_getfield('email')} - {assign_roles}")
 
             # Get user info.
             info = oidc.user_getinfo([
@@ -66,7 +79,7 @@ class AuthOIDCView(AuthOIDView):
                 user.roles.clear()
                 user.roles.extend(assign_roles)
                 sm.update_user(user)
-                print(f"----Added user {info.get('email')} with role {assign_roles}")
+                print(f"----Added user {info.get('email')} with roles {assign_roles}")
             else:
                 # Update user information
                 update_user = sm.find_user(email=info.get('email'))
