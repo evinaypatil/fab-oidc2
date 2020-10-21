7ad6570ce15932b707d796266845164f5b7f6773
patilv <vinay.x.patil.-nd@espn.com>
2020-10-21 18:43:07 +0530

    Enable accessing roles from OIDC token, and using them to grant access

--- fab_oidc/views.py
+++ fab_oidc/views.py
@@ -1,5 +1,5 @@
 import os
-from flask import redirect, request
+from flask import redirect, request, render_template_string
 from flask_appbuilder.security.views import AuthOIDView
 from flask_login import login_user
 from flask_admin import expose
@@ -26,21 +26,62 @@ class AuthOIDCView(AuthOIDView):
         def handle_login():
             user = sm.auth_user_oid(oidc.user_getfield('email'))
 
-            if user is None:
-                info = oidc.user_getinfo([
-                    USERNAME_OIDC_FIELD,
-                    FIRST_NAME_OIDC_FIELD,
-                    LAST_NAME_OIDC_FIELD,
-                    'email',
-                ])
+            # Get user roles.
+            user_roles = oidc.user_getfield('roles')
+            print(f"----User {oidc.user_getfield('email')} has roles in JWT - {user_roles}")
 
+            # Iterate through each role, and check if its available.
+            assign_roles = []
+            if user_roles:
+                for role in user_roles:
+                    fetch_role = sm.find_role(role)
+                    if fetch_role:
+                        assign_roles.append(fetch_role)
+
+            # Thrown 401 if no roles are assigned to the user
+            if len(assign_roles) == 0:
+                print(f"No role available for the user {oidc.user_getfield('email')} for access...")
+                return render_template_string("Unauthorized Access! Please contact administrator...")
+            else:
+                print(f"----Roles available for the user {oidc.user_getfield('email')} - {assign_roles}")
+
+            # Get user info.
+            info = oidc.user_getinfo([
+                USERNAME_OIDC_FIELD,
+                FIRST_NAME_OIDC_FIELD,
+                LAST_NAME_OIDC_FIELD,
+                'email',
+            ])
+
+            # Add user, if not in system, else update user
+            if user is None:
                 user = sm.add_user(
                     username=info.get(USERNAME_OIDC_FIELD),
                     first_name=info.get(FIRST_NAME_OIDC_FIELD),
                     last_name=info.get(LAST_NAME_OIDC_FIELD),
                     email=info.get('email'),
-                    role=sm.find_role(sm.auth_user_registration_role)
+                    role=assign_roles[0]
                 )
+
+                user.roles.clear()
+                user.roles.extend(assign_roles)
+                sm.update_user(user)
+                print(f"----Added user {info.get('email')} with role {assign_roles}")
+            else:
+                # Update user information
+                update_user = sm.find_user(email=info.get('email'))
+
+                update_user.username = info.get(USERNAME_OIDC_FIELD)
+                update_user.first_name = info.get(FIRST_NAME_OIDC_FIELD)
+                update_user.last_name = info.get(LAST_NAME_OIDC_FIELD)
+                update_user.email = info.get('email')
+                update_user.active = True
+
+                update_user.roles.clear()
+                update_user.roles.extend(assign_roles)
+
+                sm.update_user(update_user)
+                print(f"----Updated User {info.get('email')} with roles - {assign_roles}")
 
             login_user(user, remember=False)
             return redirect(self.appbuilder.get_url_for_index)
