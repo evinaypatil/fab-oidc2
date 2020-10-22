6cb50e91c96ae01e74a1e481308abaa379b8867f
Vishnu Prasad S <vishnu667@gmail.com>
2020-10-22 13:41:21 +0530

    0.0.1 Version release (#1)
    *Enabling Roles from JWT token
    
    Co-authored-by: evinaypatil <evinaypatil@gmail.com>

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Wrapper for [flask_oidc] that exposes a `SecurityManager` for use with any Flask-AppBuilder app.
 
-It will allow your users to login with OpenIDConnect providers such as Auth0, Okta or Google Apps.
+It will allow your users to login with OpenIDConnect providers such as Auth0, Okta or Google Apps. This version of the code extracts the roles information of OIDC ID token, and allow a request to continue only if the role is available on the application.
 
 This is roughly inspired by the code in this [stackoverflow](https://stackoverflow.com/a/47787279/44252) answer. (MIT Licenced © [thijsfranck](https://stackoverflow.com/users/8905583/thijsfranck))
 
@@ -13,7 +13,7 @@ This is roughly inspired by the code in this [stackoverflow](https://stackoverfl
 Just override the default security manager in your Flask Appbuilder app.
 
 ```python
-from fab_oidc.security import OIDCSecurityManager
+from fab_oidc2.security import OIDCSecurityManager
 
 appbuilder = AppBuilder(app, db.session, security_manager_class=OIDCSecurityManager)
 ```
@@ -22,7 +22,7 @@ appbuilder = AppBuilder(app, db.session, security_manager_class=OIDCSecurityMana
 Airflow provides a hook in the `webserver_config.py` file where you can specify a security manager class.
 In `webserver_config.py` import the OIDCSecurityManager and set
 ```python
-from fab_oidc.security import AirflowOIDCSecurityManager
+from fab_oidc2.security import AirflowOIDCSecurityManager
 ...
 SECURITY_MANAGER_CLASS = AirflowOIDCSecurityManager
 ```
@@ -39,7 +39,7 @@ the config is in a file called `superset_config.py` and the hook is called
 library alongside Superset.
 
 ```python
-from fab_oidc.security import SupersetOIDCSecurityManager
+from fab_oidc2.security import SupersetOIDCSecurityManager
 ...
 CUSTOM_SECURITY_MANAGER = SupersetOIDCSecurityManager
 ```
@@ -59,20 +59,27 @@ see the [flask_oidc manual client registration][flask_oidc_manual_config] docs f
 ### OIDC Field configuration
 
 If you like to change the default OIDC field that will be used as a username,
-first name and last name you can set the following env var in the shell you run
+first name, last name and granting access via OIDC roles, you can set the following env var in the shell you run
 your process:
 
 ```bash
 export USERNAME_OIDC_FIELD='preferred_username'
 export FIRST_NAME_OIDC_FIELD='given_name'
 export LAST_NAME_OIDC_FIELD='family_name'
+export ENABLE_ROLE_OIDC_ACCESS='true'
+export ROLES_OIDC_FIELD='roles'
 ```
+
+#### Project status
+
+This is a fork of the project [ministryofjustice/fab-oidc]: https://github.com/ministryofjustice/fab-oidc
+
 
 Copyright © 2018 HM Government (Ministry of Justice Digital Services). See LICENSE.txt for further details.
 
 
-[flask_oidc]: http://flask-oidc.readthedocs.io/en/latest/
-[flask_oidc_settings]: http://flask-oidc.readthedocs.io/en/latest/#settings-reference
-[flask_oidc_manual_config]: http://flask-oidc.readthedocs.io/en/latest/#manual-client-registration
+[flask_oidc2]: http://flask-oidc.readthedocs.io/en/latest/
+[flask_oidc2_settings]: http://flask-oidc2.readthedocs.io/en/latest/#settings-reference
+[flask_oidc2_manual_config]: http://flask-oidc2.readthedocs.io/en/latest/#manual-client-registration
 [Airflow]: https://airflow.apache.org/
  [Superset]: https://superset.incubator.apache.org/
---
+++ fab_oidc2/views.py
@@ -0,0 +1,114 @@
+import os
+from flask import redirect, request, render_template_string
+from flask_appbuilder.security.views import AuthOIDView
+from flask_login import login_user
+from flask_admin import expose
+from urllib.parse import quote
+from logging import getLogger
+log = getLogger(__name__)
+
+# Set the OIDC field that should be used as a username
+USERNAME_OIDC_FIELD = os.getenv('USERNAME_OIDC_FIELD', default='sub')
+FIRST_NAME_OIDC_FIELD = os.getenv('FIRST_NAME_OIDC_FIELD',
+                                  default='nickname')
+LAST_NAME_OIDC_FIELD = os.getenv('LAST_NAME_OIDC_FIELD',
+                                 default='name')
+ENABLE_ROLE_OIDC_ACCESS = os.getenv('ENABLE_ROLE_OIDC_ACCESS',
+                                    default='True')
+ROLES_OIDC_FIELD = os.getenv('ROLES_OIDC_FIELD',
+                             default='roles')
+
+
+class AuthOIDCView(AuthOIDView):
+
+    @expose('/login/', methods=['GET', 'POST'])
+    def login(self, flag=True):
+
+        sm = self.appbuilder.sm
+        oidc = sm.oid
+
+        @self.appbuilder.sm.oid.require_login
+        def handle_login():
+            user = sm.auth_user_oid(oidc.user_getfield('email'))
+
+            # Get user roles.
+            user_roles = oidc.user_getfield(ROLES_OIDC_FIELD)
+            
+            # Iterate through each role, and check if its available.
+            assign_roles = []
+            if ENABLE_ROLE_OIDC_ACCESS.lower() in ['true']:
+                if user_roles:
+                    for role in user_roles:
+                        fetch_role = sm.find_role(role)
+                        if fetch_role:
+                            assign_roles.append(fetch_role)
+            else:
+                if sm.auth_user_registration_role:
+                    user_reg_role = sm.find_role(sm.auth_user_registration_role)
+                    if user_reg_role:
+                        assign_roles.append(sm.find_role(sm.auth_user_registration_role))
+
+            # Thrown 401 if no roles are assigned to the user
+            if len(assign_roles) == 0:
+                log.debug(f"No role available for the user {oidc.user_getfield('email')} for access...")
+                return render_template_string("Unauthorized Access! Please contact administrator...")
+
+            # Get user info.
+            info = oidc.user_getinfo([
+                USERNAME_OIDC_FIELD,
+                FIRST_NAME_OIDC_FIELD,
+                LAST_NAME_OIDC_FIELD,
+                'email',
+            ])
+
+            # Add user, if not in system, else update user
+            if user is None:
+                user = sm.add_user(
+                    username=info.get(USERNAME_OIDC_FIELD),
+                    first_name=info.get(FIRST_NAME_OIDC_FIELD),
+                    last_name=info.get(LAST_NAME_OIDC_FIELD),
+                    email=info.get('email'),
+                    role=assign_roles[0]
+                )
+
+                user.roles.clear()
+                user.roles.extend(assign_roles)
+                sm.update_user(user)
+                log.debug(f"New User added {info.get('email')} with roles {assign_roles}")
+            else:
+                # Update user information
+                update_user = sm.find_user(email=info.get('email'))
+
+                update_user.username = info.get(USERNAME_OIDC_FIELD)
+                update_user.first_name = info.get(FIRST_NAME_OIDC_FIELD)
+                update_user.last_name = info.get(LAST_NAME_OIDC_FIELD)
+                update_user.email = info.get('email')
+                update_user.active = True
+
+                update_user.roles.clear()
+                update_user.roles.extend(assign_roles)
+
+                sm.update_user(update_user)
+                log.debug(f"Logged in User {info.get('email')} with roles {assign_roles}")
+
+            login_user(user, remember=False)
+            return redirect(self.appbuilder.get_url_for_index)
+
+        return handle_login()
+
+    @expose('/logout/', methods=['GET', 'POST'])
+    def logout(self):
+
+        oidc = self.appbuilder.sm.oid
+
+        oidc.logout()
+        super(AuthOIDCView, self).logout()
+        redirect_url = request.url_root.strip(
+            '/') + self.appbuilder.get_url_for_login
+
+        logout_uri = oidc.client_secrets.get(
+            'issuer') + '/protocol/openid-connect/logout?redirect_uri='
+        if 'OIDC_LOGOUT_URI' in self.appbuilder.app.config:
+            logout_uri = self.appbuilder.app.config['OIDC_LOGOUT_URI']
+
+        return redirect(logout_uri + quote(redirect_url))
